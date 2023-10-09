0ff9871d8b9a8c1e269c6a294b56f05c13eb7c71
hcollins345 <hcollins345@gmail.com>
2023-10-09 19:55:22 +0100

    Update to swap tab/esc on admin programs as well

--- windows_startup/uncap_script.bat
+++ windows_startup/uncap_script.bat
@@ -5,4 +5,6 @@
 ::	created a shortcut for this file by right clicking in the startup directory and putting
 :: powershell.exe -command "& 'C:\Users\Hcollins\OneDrive` -` Intel` Corporation\dotfiles\windows_startup\uncap_script.ps1'"
 :: in the shortcut box
+Powershell -Command "& Start-Process \"%USERPROFILE%\dotfiles\windows_startup\uncap.exe\" -ArgumentList @(\"0x1B:0x14\", \"0x14:0x1B\") -Verb RunAs"
 %USERPROFILE%\dotfiles\windows_startup\uncap.exe 0x1B:0x14 0x14:0x1B
+PAUSE
