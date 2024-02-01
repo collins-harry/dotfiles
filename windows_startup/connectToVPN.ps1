be5df242a098fb58dfe134e1940a78be4d4a7f6e
hcollins345 <hcollins345@gmail.com>
2024-02-01 12:13:25 +0000

    Detect if ter already open in workStart

--- windows_startup/connectToVPN.ps1
+++ windows_startup/connectToVPN.ps1
@@ -1,7 +1,8 @@
 . "$HOME\settings.ps1"
-
 $email = Get-Email
 $access = Get-Access
+
+Write-Output "Connecting to Work VPN" -ForegroundColor Green
 
 function Connect-VPN {
   param(
@@ -21,19 +22,19 @@ While ($True) {
   # (the names of the wifi networks at work)
   $wifiName = (netsh wlan show interfaces | findstr "SSID")
   if ($wifiName -match "Purple" -or $wifiName -match "Teal" -or $wifiName -match "Grey") {
-    Write-Output "Connected to work wifi, skipping VPN connection"
+    Write-Output "  Already complete, skipping"
     break
   }
-  Write-Output "Connecting to VPN"
+  Write-Output "  Connecting to VPN"
   $result = Connect-VPN "Hamilton UK VPN" $email $access
   if ($result | findstr "Command completed successfully") {
-    Write-Output "Connected to VPN succesfully"
-    Write-Output "Result: $result"
+    Write-Output "    Connected to VPN succesfully"
+    Write-Output "    Result: $result"
     break
   }
   else {
-    Write-Output "Connection failed, trying again in 5 seconds"
-    Write-Output "Error: $result"
+    Write-Output "    Connection failed, trying again in 5 seconds"
+    Write-Output "    Error: $result"
     Start-Sleep -Seconds 5 # wait for 5 seconds before the next attempt
   }
 }
--- windows_startup/startWindowsTerminal.ps1
+++ windows_startup/startWindowsTerminal.ps1
@@ -1,3 +1,7 @@
 write-host "Starting Windows Terminal with WSL" -ForegroundColor Green
-Start-Process -FilePath "wt.exe" -ArgumentList "new-tab ; new-tab -p Ubuntu" -Verb RunAs
+if (Get-Process -Name WindowsTerminal -ErrorAction SilentlyContinue) {
+  Write-Host "  Already complete, skipping"
+} else {
+  Start-Process -FilePath "wt.exe" -ArgumentList "new-tab ; new-tab -p Ubuntu" -Verb RunAs
+}
 
