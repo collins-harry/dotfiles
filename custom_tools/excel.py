f5d1c32af15b809e071cd864b20647651de11a49
hcollins345 <hcollins345@gmail.com>
2024-08-07 10:48:18 +0100

    Update with condFormats, freezePanes and colwidth

--- custom_tools/excel.py
+++ custom_tools/excel.py
@@ -12,17 +12,30 @@
                 {
                     "worksheet_title": "",
                     "heading note": "Contact Harry Collins,if you have any questions",
-                    "headings": ,
-                    "values": ,
+                    "headings": [
+                        ""
+                        ],
+                    "merge_headers": [
+                        {"level":1, "cell_range":["E","I"], "text":"Name", "format":"dark_blue_center"},
+                    ],
+                    "values": df[[
+                        "",
+                        ]].values.tolist(),
+                    "conditional_formatting": [ 
+                        {"range": "A1:X9999", "type": "cell", "criteria": "==", "value": '"FALSE"', "format": "conditional_red"},
+                        {"range": "A1:X9999", "type": "cell", "criteria": "==", "value": '"TRUE"',  "format": "conditional_green"},
+                    ],
                     "right_borders": ["A","D","H","K","N"],
                     "tab_color": "red",
-                    "final_row_color": "light_green"
+                    "final_row_color": "light_green",
+                    "set_column_width": True,
+                    "freeze_headers": True,
                 },
                 ]
             }
 
-class Excel():
-    """V5 from edc2kill"""
+class excel():
+
     def __init__(self):
         pass
 
@@ -39,23 +52,40 @@ class Excel():
             formats = self._add_styles_to_workbook(workbook)
             for table in tables["sheets"]:
                 worksheet = workbook.add_worksheet(table["worksheet_title"])
-                if heading_note := table.get("heading note"):
+                if heading_note := table.get("heading_note"):
                     worksheet.write(0, 0, heading_note)
                     startrow = 1
                 else:
                     startrow = 0
+                if merge_headers := table.get("merge_headers"):
+                    max_level = 0
+                    for merge in merge_headers:
+                        row = startrow + merge["level"]
+                        if merge["level"] > max_level:
+                            max_level = merge["level"]
+                        if merge['cell_range'][0] == merge['cell_range'][1]:
+                            worksheet.write(f"{merge['cell_range'][0]}{row}", merge["text"], formats[merge["format"]])
+                        else:
+                            cell_range = f"{merge['cell_range'][0]}{row}:{merge['cell_range'][1]}{row}"
+                            worksheet.merge_range(cell_range, merge["text"], formats[merge["format"]])
+                    startrow = startrow + max_level
                 right_border_indices = self._convert_letters_to_indices(right_borders) if (right_borders := table.get("right_borders")) else []
-                if tab_color := table.get("tab_color"):
-                    worksheet.set_tab_color(tab_color)
                 for column, heading in enumerate(table["headings"]):
                     worksheet.write(startrow, column, heading, formats[f"light_blue{'_right_border' if column in right_border_indices else ''}"])
+                startrow += 1
                 for row_index, row_values in enumerate(table["values"]):
                     for column_index, value in enumerate(row_values):
                         if (color := table.get("final_row_color")) and (row_index == len(table["values"])-1):
-                            worksheet.write(startrow + 1 + row_index, column_index, value, formats[f"{color}{'_right_border' if column_index in right_border_indices else ''}"])
+                            worksheet.write(startrow + row_index, column_index, value, formats[f"{color}{'_right_border' if column_index in right_border_indices else ''}"])
                         else:
-                            worksheet.write(startrow + 1 + row_index, column_index, value, formats[f"none{'_right_border' if column_index in right_border_indices else ''}"])
-                self._set_column_width(worksheet, table["headings"], table["values"])
+                            worksheet.write(startrow + row_index, column_index, value, formats[f"none{'_right_border' if column_index in right_border_indices else ''}"])
+                if tab_color := table.get("tab_color"):
+                    worksheet.set_tab_color(self.style_lookup[tab_color]["bg_color"])
+                if table.get("set_column_width", True):
+                    self._set_column_width(worksheet, table["headings"], table["values"])
+                if table.get("freeze_headers", False):
+                    worksheet.freeze_panes(startrow, 0)
+                self._set_conditional_formatting(worksheet, table["conditional_formatting"], formats)
 
     def _convert_letters_to_indices(self, columns):
         return [string.ascii_uppercase.index(column_letter) for column_letter in columns]
@@ -106,10 +136,24 @@ class Excel():
         for column_index, column_width in enumerate(max_lengths):
             worksheet.set_column(column_index, column_index, column_width + 1)
 
+    def _set_conditional_formatting(self, worksheet, conditional_formats, formats):
+        for conditional_format in conditional_formats:
+            if conditional_format["type"] == "formula":
+                worksheet.conditional_format(conditional_format["range"], {"type": conditional_format["type"], 
+                                                                           "criteria": conditional_format["criteria"], 
+                                                                           "format": formats[conditional_format["format"]]})
+            else:
+                worksheet.conditional_format(conditional_format["range"], {"type": conditional_format["type"], 
+                                                                           "criteria": conditional_format["criteria"], 
+                                                                           "value": conditional_format["value"],
+                                                                           "format": formats[conditional_format["format"]]})
+
     def _add_styles_to_workbook(self, workbook):
-        style_lookup = {
+        self.style_lookup = {
             "dark_blue": {"bg_color": "#b4c6e7"},
+            "dark_blue_center": {"bg_color": "#b4c6e7", "align":"center", "right":True, "left":True},
             "light_blue": {"bg_color": "#d9e1f2"},
+            "light_blue_center": {"bg_color": "#d9e1f2", "align":"center", "right":True, "left":True},
             "dark_blue_right_border": {"bg_color": "#b4c6e7", "right": True},
             "light_blue_right_border": {"bg_color": "#d9e1f2", "right": True},
             "light_grey_all_border": {"bg_color": "#e7e6e6", "border": True},
@@ -127,9 +171,18 @@ class Excel():
             "dark_yellow": {"bg_color": "#ffeb9c"},
             "red": {"bg_color": "#ffc7ce"},
             "red_right_border": {"bg_color": "#ffc7ce", "border":True},
+            "conditional_red": {'bg_color': '#FFC7CE', 'font_color': '#9C0006'},
+            "conditional_yellow": {'bg_color': '#FFEB9C', 'font_color': '#9C6500'},
+            "conditional_green": {'bg_color': '#C6EFCE', 'font_color': '#006100'},
+            "pastel_green": {"bg_color": "#D8E4BC"},
+            "pastel_purple": {"bg_color": "#CCC0DA"},
+            "pastel_blue": {"bg_color": "#B8CCE4"},
+            "pastel_red": {"bg_color": "#E6B8B7"},
+            "percentage": {'num_format': '0.00%'},
         }
-        return {name: workbook.add_format(style) for name, style in style_lookup.items()}
+        return {name: workbook.add_format(style) for name, style in self.style_lookup.items()}
 
     def open_excel(self):
         file_path = self.output_path
         subprocess.Popen(str(file_path), shell=True)
+
