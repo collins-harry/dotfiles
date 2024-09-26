757e876ea773f7c0fa553db5d47a3186e8d2a5b8
hcollins345 <hcollins345@gmail.com>
2024-09-26 12:27:36 +0100

    Try connecting to both VPN, improved Work/VPN detection

--- windows_startup/connectToVPN.ps1
+++ windows_startup/connectToVPN.ps1
@@ -16,31 +16,32 @@ function Connect-VPN {
   return rasdial $vpnName $email $access
 }
 
+$vpns = @('Hamilton IE VPN', 'Hamilton UK VPN')
+$vpnIndex = 0
 
 While ($True) {
-  if (Get-NetIpConfiguration | findstr "hamiltongroupuk.com") {
-    Write-Host "  Already connected to Work VPN/WIFI/LAN, skipping"
-    break
-  }
-  # Check if wifi is connected to "Purple", "Teal" or "Grey" 
-  # (the names of the wifi networks at work)
-  # $wifiName = (netsh wlan show interfaces | findstr "SSID")
-  # if ($wifiName -match "Purple" -or $wifiName -match "Teal" -or $wifiName -match "Grey") {
-  #   Write-Host "  Already complete, skipping"
-  #   break
-  # }
-  Write-Host "  Connecting to VPN"
-  $result = Connect-VPN "Hamilton UK VPN" $email $access
-  if ($result | findstr "Command completed successfully") {
-    Write-Host "    Connected to VPN succesfully"
-    Write-Host "    Result: $result"
-    break
-  }
-  else {
-    Write-Host "    Connection failed, trying again in 5 seconds"
-    Write-Host "    Error: $result"
-    Start-Sleep -Seconds 5 # wait for 5 seconds before the next attempt
-  }
+    if (Get-NetIpConfiguration | Out-String | Select-String "hamiltongroupuk.com") {
+        Write-Host "  Already connected to Work VPN/WIFI/LAN, skipping"
+        break
+    }
+
+    $currentVpn = $vpns[$vpnIndex]
+    Write-Host "  Attempting to connect to $currentVpn"
+    Write-Host "Connect-VPN $currentVpn $email $access"
+    $result = Connect-VPN $currentVpn $email $access
+    
+
+    if ($result | Select-String "Command completed successfully") {
+        Write-Host "    Connected to $currentVpn successfully"
+        Write-Host "    Result: $result"
+        break
+    }
+    else {
+        Write-Host "    Connection to $currentVpn failed, trying the other VPN in 5 seconds"
+        Write-Host "    Error: $result"
+        Start-Sleep -Seconds 2 # wait for 5 seconds before the next attempt
+        $vpnIndex = ($vpnIndex + 1) % 2 # Switch to the other VPN
+    }
 }
 
 exit 0
