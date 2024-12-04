a5e4c3ae316a7908bad8508b7343b6c47fcd0188
hcollins345 <hcollins345@gmail.com>
2024-12-04 13:28:25 +0000

    Update workStart with repo password scrap

--- windows_startup/workStart/workStart.ps1
+++ windows_startup/workStart/workStart.ps1
@@ -1,5 +1,6 @@
 . "$HOME\dotfiles\windows_startup\workStart\connectToVPN.ps1"
 . "$HOME\extract_token.ps1"
+. "$HOME\extract_repo_password.ps1"
 . "$HOME\dotfiles\windows_startup\workStart\startWindowsTerminal.ps1"
 # wait 10 seconds for terminal to start
 write-host "Waiting 10 seconds to make sure terminal started" -ForegroundColor Green
